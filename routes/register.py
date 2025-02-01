5325bc2f5b04cb7fbd00f263a007608afcab3190
Md-Jannatul-Ferdouse-Rahat-Ibne-Yousuf <jannatul.ferdous@bjitacademy.com>
2025-02-01 15:23:31 +0600

    Fix(Bug): Logout after registration

--- routes/register.py
+++ routes/register.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 
 import crud
 from database import get_db
+from utils import *
+
 
 # Define templates for rendering HTML
 templates = Jinja2Templates(directory="templates")
@@ -42,6 +44,7 @@ def register_user(
     license_number: str = Form(None),  
     experience_years: str = Form(None),
     db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user)
 ):
     try:
         user = crud.create_user(
@@ -60,8 +63,14 @@ def register_user(
         elif role_id == 3:
             crud.create_driver(db, user_id, license_number, experience_years)
 
+        user = crud.get_user_by_username(db, current_user["username"])
+        user_id = user["id"]
+        
+        # Fetch roles of the current user
+        roles = crud.get_user_roles(db, user_id)
+        role_names = [role['name'] for role in roles]
         return templates.TemplateResponse(
-        "index.html", {"request": request}
+        "index.html", {"request": request, "current_user": user, "role_names": role_names}
     )
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
